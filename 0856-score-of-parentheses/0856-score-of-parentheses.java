652cb4830105cb65c4efeacb78540295a596399c
raunakpathak <raunakpathak2002@gmail.com>
2026-08-11 18:05:16 +0530

    Time: 0 ms (100%), Space: 42.8 MB (51.55%) - LeetHub

---
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int scoreOfParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+
+        int count = 0 ;
+
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s.charAt(i) == '('){
+                stack.push(count);
+                count = 0 ;
+            }else{
+                count = Math.max(2 * count , 1) + stack.pop();
+            }
+        }
+        return count;
+    }
+}
