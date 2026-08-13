ea1fae5d9443fcb5b39dc56c4f1382bb22c00b53
raunakpathak <raunakpathak2002@gmail.com>
2026-08-13 17:25:34 +0530

    Time: 15 ms (46.7%), Space: 47.8 MB (40.4%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countCollisions(String directions) {
+        int n =  directions.length();
+        int left = 0 ; 
+        int right = n -1 ;
+        int count = 0 ;
+        while(left < n && directions.charAt(left) == 'L')left++;
+        while(right >= 0 && directions.charAt(right) == 'R' )right--;
+
+        for(int i = left ; i <= right ; i++){
+            char c = directions.charAt(i);
+
+            if(c == 'L' || c == 'R'){
+                count++;
+            }
+        }
+        return count;
+    }
+}
