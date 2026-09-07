01697cb98a43a19cc48e9536a8a9e9d094cd281a
raunakpathak <raunakpathak2002@gmail.com>
2026-09-07 23:38:21 +0530

    Time: 0 ms (100%), Space: 43.1 MB (38.08%) - LeetHub

---
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        int n = nums.length ;
+
+        if(n <  2)return n ;
+
+        int up  = 1 ;
+        int down = 1;
+
+        for(int i = 1 ; i < nums.length ; i++){
+            if(nums[i] > nums[i-1]){
+                up = down + 1 ;
+            }
+            else if(nums[i] < nums[i -1]){
+                down = up + 1 ;
+            }
+        }
+        return Math.max(up , down );
+    }
+}
